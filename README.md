b482bd1423b14d909806e16d81637abb2aedf2fb
Marco Bacis <marc.bacis@gmail.com>
2025-07-01 17:48:40 +0200

    feat: add action script and yaml

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# goodreads-scrape-action
-Action to scrape a specific goodreads books list and export the data as json
+# Goodreads Scrape Action
+
+Action to scrape a specific goodreads books shelf and export the data as json
+
+
+## Usage
+
+Example usage in workflow:
+```yaml
+name: Update books list
+on:
+  schedule:
+    - cron: '0 0 * * *'
+permissions:
+  contents: write 
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+      # checkout and do other stuff
+
+      - uses: marcobacis/goodreads-scrape-actions@v0.1.0
+        with:
+          user: "22830084"
+          shelf: "currently-reading"
+          output_path: "output.json"
+    
+      # other, e.g. commit output file
+```
---
+++ src/scrape_books.py
@@ -0,0 +1,49 @@
+import json
+import sys
+from datetime import datetime, date
+
+import goodreads
+
+if len(sys.argv) <= 3:
+    print("Usage: {} <user> <shelf> <output-path>".format(sys.argv[0]))
+    sys.exit("Wrong number of arguments")
+
+def load_existing(path):
+    try:
+        with open(output_path, "r", encoding="utf-8") as f:
+            return json.load(f)["books"] or []
+    except FileNotFoundError:
+        return [] 
+    except json.JSONDecodeError:
+        print("Warning: existing file is not valid JSON. It will be overwritten.")
+        return []
+
+def normalized_json(content):
+    def normalize_book(book):
+        return {
+            k: v.isoformat() if isinstance(v, (date, datetime)) else v
+            for k, v in book.items()
+        }
+
+    normalized = sorted([normalize_book(b) for b in content], key=lambda b: b["title"])
+    return json.dumps(normalized, ensure_ascii=False, sort_keys=True, indent=2)
+
+user = sys.argv[1]
+shelf = sys.argv[2]
+output_path = sys.argv[3]
+
+old_books = load_existing(output_path)
+books = goodreads.get_books(user, shelf)
+
+if normalized_json(old_books) == normalized_json(books):
+    print("No Changes detected. File not updated")
+    sys.exit(0)
+
+# Write the new data
+with open(output_path, "w", encoding="utf-8") as f:
+    new_data = {
+        "lastmod": datetime.now().isoformat(),
+        "books": books,
+    }
+    json.dump(new_data, f, ensure_ascii=False, sort_keys=True, indent=2, default=str)
+    print(f"File '{output_path}' updated.")
